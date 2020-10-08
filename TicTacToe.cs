34618f0e5d6a2ae1e269f70b0373acd364bcaa4f
MukheshRavi <mukeshparvathi@gmail.com>
2020-10-08 11:16:42 +0530

    Show board added

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -9,11 +9,16 @@ namespace TicTocGame
             TicTacToe ticTacToe = new TicTacToe();
             Console.WriteLine("Welcome to TicTacToeGame");
             char [] ticTacToeBoard =ticTacToe.createBoard();
-            ticTacToe.chooseSymbol();
+            char playerSymbol=ticTacToe.chooseSymbol();
+            ticTacToe.showBoard();
 
         }
 
         public char[] board = new char[10];
+       /// <summary>
+       /// This method is to create board
+       /// </summary>
+       /// <returns></returns>
         public char [] createBoard()
         {
             for (int i = 0; i < board.Length; i++)
@@ -21,7 +26,11 @@ namespace TicTocGame
 
             return board;
         }
-        public void chooseSymbol()
+        /// <summary>
+        /// This method is used to Choose players Symbol
+        /// </summary>
+        /// <returns></returns>
+        public char chooseSymbol()
         {
 
             Console.WriteLine("Enter your choice to choose Symbol 1:O 2:X");
@@ -30,15 +39,32 @@ namespace TicTocGame
             {
                 case 1:
                     char playerSymbol = 'O';
-                    Console.WriteLine("you choose symbol" + playerSymbol);
-                    break;
+                    Console.WriteLine("you choose symbol " + playerSymbol);
+                    return playerSymbol;
+
                 case 2:
                     playerSymbol = 'X';
-                    Console.WriteLine("you choose symbol" + playerSymbol);
-                    break;
+                    Console.WriteLine("you choose symbol " + playerSymbol);
+                    return playerSymbol;
+                default:
+                    Console.WriteLine("choice is wrong");
+                    playerSymbol = ' ';
+                    return playerSymbol;
+
+            }
+        }
+
+            public void showBoard()
+            {
+                Console.WriteLine(board[1] + "|" + board[2] + "|" + board[3]);
+                Console.WriteLine("--" + " " + "--" + " " + "--");
+                Console.WriteLine(board[4] + "|" + board[5] + "|" + board[6]);
+                Console.WriteLine("--" + " " + "--" + " " + "--");
+                Console.WriteLine(board[7] + "|" + board[8] + "|" + board[9]);
             }
 
-        }
+
+        
 
     }
 
